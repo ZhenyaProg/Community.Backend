3aa45317c6c418335870c148316734932c7f367e
Zhenya <schoolbast@mail.ru>
2025-03-25 22:16:10 +0300

    add cors support

--- Community/Community.API/Program.cs
+++ Community/Community.API/Program.cs
@@ -13,10 +13,21 @@ internal class Program
 
         builder.Services.AddControllers();
 
+        builder.Services.AddCors(options =>
+        {
+            options.AddDefaultPolicy(policy =>
+            {
+                policy.WithOrigins("http://127.0.0.1:5500");
+                policy.AllowAnyHeader();
+                policy.AllowAnyMethod();
+            });
+        });
+
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
 
+        app.UseCors();
         app.UseHttpsRedirection();
 
         app.MapControllers();
